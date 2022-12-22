258911625ca7e3c846c46c6492e69b33eb4eb6d3
Chad <chadjcampbell@gmail.com>
2022-12-22 15:29:01 -0500

    hooked up seraching spinner and class

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -31,6 +31,10 @@ export default function Root() {
     document.getElementById("q")!.value = q;
   }, [q]);
 
+  const searching =
+    navigation.location &&
+    new URLSearchParams(navigation.location.search).has("q");
+
   return (
     <>
       <div id="sidebar">
@@ -39,16 +43,20 @@ export default function Root() {
           <Form id="search-form" role="search">
             <input
               id="q"
+              className={searching ? "loading" : ""}
               aria-label="Search contacts"
               placeholder="Search"
               type="search"
               name="q"
               defaultValue={q}
               onChange={(event) => {
-                submit(event.currentTarget.form);
+                const isFirstSearch = q == null;
+                submit(event.currentTarget.form, {
+                  replace: !isFirstSearch,
+                });
               }}
             />
-            <div id="search-spinner" aria-hidden hidden={true} />
+            <div id="search-spinner" aria-hidden hidden={!searching} />
             <div className="sr-only" aria-live="polite"></div>
           </Form>
           <Form method="post">
