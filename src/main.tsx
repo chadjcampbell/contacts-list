ea7fbd813cc933ca131976796ffda3b6cc489928
Chad <chadjcampbell@gmail.com>
2022-12-26 18:42:10 -0500

    add error handling to contact path

--- src/main.tsx
+++ src/main.tsx
@@ -30,6 +30,7 @@ const router = createBrowserRouter([
       {
         path: "contacts/:contactId",
         element: <Contact />,
+        errorElement: <ErrorPage />,
         loader: contactLoader,
         action: contactAction,
       },
